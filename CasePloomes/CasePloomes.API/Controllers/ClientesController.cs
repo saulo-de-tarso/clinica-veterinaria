2f5072787ef2ab01d6d927b35f8783dd69578a66
Saulo Campos <saulodtrc@gmail.com>
2024-04-20 13:50:44 -0300

    Criada API com controlador de clientes retornando a frase 'Cliente criado', e as entidades clientes, produtos e vendas com seus atributos

---
+++ CasePloomes/CasePloomes.API/Controllers/ClientesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasePloomes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpPost("criar")]
+        public string Create()
+        {
+            return "Cliente criado.";
+        }
+    }
+}
---
+++ CasePloomes/CasePloomes.API/Entities/VendasEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CasePloomes.API.Entities
+{
+    public class VendasEntity
+    {
+        [Key]
+        [Required(ErrorMessage = "O ID do cliente é obrigatório.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "O valor deve ser um número inteiro.")]
+        [Range(1, int.MaxValue, ErrorMessage = "O valor deve ser maior ou igual a 1.")]
+        public int IdCliente { get; set; }
+
+        [Required(ErrorMessage = "O código do produto é obrigatório.")]
+        [StringLength(50, ErrorMessage = "O nome deve ter no máximo {1} caracteres.")]
+        public string CodigoProduto { get; set; }
+
+        [Required(ErrorMessage = "A quantidade é obrigatória.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "O valor deve ser um número inteiro.")]
+        [Range(1, int.MaxValue, ErrorMessage = "O valor deve ser maior ou igual a 1.")]
+        public int Quantidade { get; set; }
+
+    }
+}
